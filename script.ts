23dcc6fe7d6801db38cf37c4b50d7eede9935fca
vito <nklsvito@gmail.com>
2025-03-23 21:44:05 -0300

    updated script

--- script.ts
+++ script.ts
@@ -1,1 +1,7 @@
 
+window.onload = () => {
+    const img = document.createElement('img');
+    img.src = 'images/undercons.png';
+    img.alt = 'Under Construction';
+    document.body.appendChild(img);
+};
