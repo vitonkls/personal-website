64bdf465aab3f6e4e85c9b3e8e357401ec9924bb
vito <nklsvito@gmail.com>
2025-03-23 21:47:53 -0300

    updated js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     var img = document.createElement('img');
     img.src = 'images/undercons.png';
-    img.alt = 'Under Construction';
+    img.alt = 'vitonkls.com under construction';
     document.body.appendChild(img);
 };
--- script.ts
+++ script.ts
@@ -2,6 +2,6 @@
 window.onload = () => {
     const img = document.createElement('img');
     img.src = 'images/undercons.png';
-    img.alt = 'Under Construction';
+    img.alt = 'vitonkls.com under construction';
     document.body.appendChild(img);
 };
