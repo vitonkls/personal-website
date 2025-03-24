719ef218092c3ecea067adba5158eb1b4255655f
vito <nklsvito@gmail.com>
2025-03-23 21:59:34 -0300

    updated js and ts

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     console.log('Script Loaded');
     var img = document.createElement('img');
-    img.src = 'images/uncdcons.png';
+    img.src = '/images/uncdcons.png';
     img.alt = 'vitonkls.com under construction';
     document.body.appendChild(img);
 };
--- script.ts
+++ script.ts
@@ -2,7 +2,7 @@
 window.onload = () => {
     console.log('Script Loaded');
     const img = document.createElement('img');
-    img.src = 'images/uncdcons.png';
+    img.src = '/images/uncdcons.png';
     img.alt = 'vitonkls.com under construction';
     document.body.appendChild(img);
 };
