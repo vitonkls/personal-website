aa0737569fda8869962bca361b1c5b28abc338b1
vito <nklsvito@gmail.com>
2025-03-23 21:46:58 -0300

    updated ts

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    var img = document.createElement('img');
+    img.src = 'images/undercons.png';
+    img.alt = 'Under Construction';
+    document.body.appendChild(img);
+};
